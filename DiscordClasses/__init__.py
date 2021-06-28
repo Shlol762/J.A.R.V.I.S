df9873e9f1a44bbb5ab3ae5d249a6b6e9bc72d95
Shlok <wizard1net@gmail.com>
2021-06-28 12:39:12 +0530

    Removed sensitive bot info

--- DiscordClasses/__init__.py
+++
@@ -1,8 +0,0 @@
-from .codec import *
-from .custom_funcs import *
-from .embeds import *
-from .version import *
-from .web_scrapers import *
-from .message_funcs import *
-
-bot_token: str = "NzQ5ODMwNjM4OTgyNTI5MDY1.X0xsMw._nyqB1zFTsAj3CrycNlzXu86UXQ"
