6aca5446ab2726e40ee58e27179e170666cb8c69
Shlok <wizard1net@gmail.com>
2021-06-28 12:57:09 +0530

    Added init file back with redacted bot token

---
+++ DiscordClasses/__init__.py
@@ -0,0 +1,9 @@
+from .codec import *
+from .custom_funcs import *
+from .embeds import *
+from .version import *
+from .web_scrapers import *
+from .message_funcs import *
+
+with open("C:/Users/Shlok/Documents/-... --- - ! - --- -.- . -..txt", "r") as f:
+    bot_token: str = f.read()
