4dfc48b3e9532f1835a306e0a77c71818fff38e3
Shlok <wizard1net@gmail.com>
2021-08-11 16:55:25 +0530

    Refactored `bot_token` to adhere to PEP configuration.

--- DiscordClasses/__init__.py
+++ DiscordClasses/__init__.py
@@ -1,11 +1,11 @@
 from .codec import *
 from .custom_funcs import *
 from .embeds import *
+from .message_funcs import *
 from .version import *
 from .web_scrapers import *
-from .message_funcs import *
 
 with open("C:/Users/Shlok/bot_stuff/-... --- - ! - --- -.- . -..txt", "r") as f:
-    bot_token: str = f.read()
+    BOT_TOKEN: str = f.read()
 
 __discord__ = discord.__version__
