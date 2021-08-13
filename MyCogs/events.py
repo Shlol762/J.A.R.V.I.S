def4d5b7750305ca7b8c4982acce575e011b48ac
Shlok <wizard1net@gmail.com>
2021-08-13 09:05:06 +0530

    Fixed error that prevented from encoding emojis in message.

--- MyCogs/events.py
+++ MyCogs/events.py
@@ -131,10 +131,10 @@ class Events(Cog):
                             if birthdays.get(str(person.id)):
                                 time: datetime.datetime = datetime.datetime.strptime(birthdays[str(person.id)]+datetime.datetime.now().strftime("/%Y"), "%d/%m/%Y")
                                 if time < datetime.datetime.now():
-                                    time: str = re.sub(" to `00:00 [0-9]{2}/[0-9]{2}/[0-9]{4}`", "", time.strftime(f"""The next occurrance of {'your' if person.id == author.id else person.name+"'s"} birthday is in {timeto(f'{time.day}/{time.month}/{time.year + 1}')} on the `%dth of %B in {time.year + 1}`"""))
+                                    time: str = re.sub(" to `00:00 [0-9]{2}/[0-9]{2}/[0-9]{4}`", "", time.strftime(f"""The next occurrance of . birthday is in {timeto(f'{time.day}/{time.month}/{time.year + 1}')} on the `%dth of %B in {time.year + 1}`"""))
                                 else:
-                                    time: str = re.sub(" to `00:00 [0-9]{2}/[0-9]{2}/[0-9]{4}`", "", time.strftime(f"""The next occurrance of {'your' if person.id == author.id else person.name+"'s"} birthday is in {timeto(f'{time.day}/{time.month}/{time.year}')} on the `%dth of %B in %Y`"""))
-                                await ctx.reply(time)
+                                    time: str = re.sub(" to `00:00 [0-9]{2}/[0-9]{2}/[0-9]{4}`", "", time.strftime(f"""The next occurrance of . birthday is in {timeto(f'{time.day}/{time.month}/{time.year}')} on the `%dth of %B in %Y`"""))
+                                await ctx.reply(time.replace(".", 'your' if person.id == author.id else person.name+"'s"))
                             else:
                                 await ctx.reply(f"I'm sorry but I don't think I have that birthday stored anywhere. Contact Shlol#2501 to add the birthday")
                         except AttributeError: pass
