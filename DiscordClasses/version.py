f0cc7a70d8ce6d2b6cc0b60a0f4756592a5989a2
Shlok <wizard1net@gmail.com>
2021-08-15 10:07:45 +0530

    Updated VCS

--- DiscordClasses/version.py
+++ DiscordClasses/version.py
@@ -1,34 +1,48 @@
+import re
+
+
 class Version:
+    __slots__ = ['q1', 'q2', 'q3']
+    __doc__ = "A class created for keeping track of the bot's VERSION."
+
     def __init__(self, vers):
-        self.__doc__ = "A class created for keeping track of the bot's VERSION."
-        self.version: str = vers
-        self.iter_stg_1: str = vers[:1]
-        self.iter_stg_2: str = vers[2:4]
-        self.iter_stg_3: str = vers[5]
+        self.q1: int = int(vers[:1])
+        self.q2: int = int(vers[2:4])
+        self.q3: int = int(vers[5])
 
-    def increment(self, iteration: str = "iter_stg_1", iter_range: int = 1):
-        """Increases the version of the bot by 1 unit."""
-        if iteration.lower() == 'iter_stg_1':
-            self.iter_stg_1 = iter_range + int(self.iter_stg_1)
-            self.version = str(self.iter_stg_1) + self.version[1:]
-        elif iteration.lower() == 'iter_stg_2':
-            self.iter_stg_2 = iter_range + int(self.iter_stg_2)
-            if self.iter_stg_2 > 99 and iter_range >= 1:
-                self.increment('iter_stg_1', 1)
-                self.iter_stg_2 = '00'
-                self.version = str(self.iter_stg_1) + "." + str(self.iter_stg_2) + "." + str(self.iter_stg_3)
+    def __str__(self):
+        return self.version
+
+    def __eq__(self, other):
+        if not isinstance(other, Version) and not isinstance(other, str):
+            raise TypeError(f'{other} of type {type(other)} cannot be compared to {repr(self)}')
+        if isinstance(other, str):
+            if re.search(r'[0-9]\.[0-9]{2}\.[0-9]', other):
+                return other == self.version
+            raise ValueError(f'Value of {other} cannot be compared to {repr(self)}')
+        return other.version == self.versions
+
+    def increment(self):
+        if 0 <= self.q3 < 9:
+            self.q3 += 1
+        else:
+            self.q3 = 10 - (self.q3 + 1)
+            if 0 <= self.q2 < 99:
+                self.q2 += 1
             else:
-                self.version = str(self.iter_stg_1) + "." + str(self.iter_stg_2) + "." + str(self.iter_stg_3)
-        elif iteration.lower() == 'iter_stg_3':
-            self.iter_stg_3 = iter_range + int(self.iter_stg_3)
-            if self.iter_stg_3 > 9 and iter_range >= 1:
-                self.increment("iter_stg_2", 1)
-                self.iter_stg_3 = '0'
-                self.version = self.version[:5] + str(self.iter_stg_3)
-            else:
-                self.version = self.version[:5] + str(self.iter_stg_3)
+                self.q2 = 100 - (self.q2 + 1)
+                self.q1 += 1
+        return self
+
+    @property
+    def version(self):
+        return f'{self.q1}.{self.q2:0>2}.{self.q3}'
 
 
-"""Used in C:/Users/Shlok/J.A.R.V.I.SV2021/Classified_J.A.R.V.I.S.py"""
-ver = Version("3.00.0")
-VERSION = ver.version
+with open("C:/Users/Shlok/bot_stuff/version.txt", 'r') as f:
+    ver = Version(f.read())
+
+VERSION = ver.increment().version
+
+with open("C:/Users/Shlok/bot_stuff/version.txt", 'w') as f:
+    f.write(VERSION)
