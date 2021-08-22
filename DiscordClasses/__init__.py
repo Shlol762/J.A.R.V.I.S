d9fd397a0a71211f7d8a528d07935055d49f9d92
Shlok <wizard1net@gmail.com>
2021-08-22 14:18:07 +0530

    bot token not here lol

--- DiscordClasses/__init__.py
+++ DiscordClasses/__init__.py
@@ -7,7 +7,7 @@ from .web_scrapers import *
 from .views import *
 
 with open("C:/Users/Shlok/bot_stuff/-... --- - ! - --- -.- . -..txt", "r") as f:
-    BOT_TOKEN: str = "NzQ5ODMwNjM4OTgyNTI5MDY1.X0xsMw.z2bU1Us_TrE0hOBMA3tsepG-zXA"
+    BOT_TOKEN: str = f.read()
 
 __discord__ = discord.__version__
 __version__ = '0.00.0'
