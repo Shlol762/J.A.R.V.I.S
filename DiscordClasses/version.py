0df50100ab07f18ae2957b191c05e7e013b95c28
Shlok <wizard1net@gmail.com>
2021-08-25 15:42:05 +0530

    Found proper naming convention for 3 components of version.

--- DiscordClasses/version.py
+++ DiscordClasses/version.py
@@ -2,13 +2,13 @@ import re
 
 
 class Version:
-    __slots__ = ['q1', 'q2', 'q3']
+    __slots__ = ['major', 'minor', 'micro']
     __doc__ = "A class created for keeping track of the bot's VERSION."
 
     def __init__(self, vers):
-        self.q1: int = int(vers[:1])
-        self.q2: int = int(vers[2:4])
-        self.q3: int = int(vers[5])
+        self.major: int = int(vers[:1])
+        self.minor: int = int(vers[2:4])
+        self.micro: int = int(vers[5])
 
     def __str__(self):
         return self.version
@@ -23,23 +23,23 @@ class Version:
             if re.search(r'[0-9]\.[0-9]{2}\.[0-9]', other):
                 return other == self.version
             raise ValueError(f'Value of {other} cannot be compared to {repr(self)}')
-        return other.version == self.versions
+        return other.version == self.version
 
     def increment(self):
-        if 0 <= self.q3 < 9:
-            self.q3 += 1
+        if 0 <= self.micro < 9:
+            self.micro += 1
         else:
-            self.q3 = 10 - (self.q3 + 1)
-            if 0 <= self.q2 < 99:
-                self.q2 += 1
+            self.micro = 10 - (self.micro + 1)
+            if 0 <= self.minor < 99:
+                self.minor += 1
             else:
-                self.q2 = 100 - (self.q2 + 1)
-                self.q1 += 1
+                self.minor = 100 - (self.minor + 1)
+                self.major += 1
         return self
 
     @property
     def version(self):
-        return f'{self.q1}.{self.q2:0>2}.{self.q3}'
+        return f'{self.major}.{self.minor:0>2}.{self.micro}'
 
 
 with open("C:/Users/Shlok/bot_stuff/version.txt", 'r') as f:
